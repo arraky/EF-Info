e79d005585b560a569c68ef2acf113d4a7987d32
arraky <dunesandspice@gmail.com>
2022-12-02 21:58:22 +0100

    Transfer to PC with following changes
    - change Field length to support numbers bigger than 100
    -change input question
    -create checker for x and y field
    - play for first time

--- numtrip/numtrip.py
+++ numtrip/numtrip.py
@@ -34,35 +34,81 @@ def midline():
     print('|')
 
 def playground():
-    fieldnum()
-    for i in range(Row):
+        fieldnum()
+        for i in range(Row):
+            line()
+            print(i,end=' ')
+            try:
+                for j in range(Coloumns):
+                    if field[i][j] >=10:
+                        len = ' '
+                    else:
+                        len = '  '
+                    if field[i][j] >=100:
+                        len = ''
+                    print(f'|  ',field[i][j], end=len)
+                
+            except:
+                print(f'|  ',field[i][j], end='  ')
+            print('|')
+
         line()
-        print(i,end=' ')
-        for j in range(Coloumns):
-            if field[i][j] >=10:
-                len = ' '
-            else:
-                len = '  '
-            print(f'|  ',field[i][j], end=len)
-        print('|')
-    line()
 
-playground()
+def X_Inputcheck(Questionx):
+    inpx = input(Questionx)
+    while not inpx.isnumeric():
+        print('Must be a number')
+        inpx = input(Questionx)
+    numx=int(inpx)
+    while numx < 1 or numx>4:
+        print('number not valid')
+        inpx = input(Questionx)
+        numx=int(inpx)
+    return numx
 
+def Y_Inputcheck(Questiony):
+    inpy = input(Questiony)
+    while not inpy.isnumeric():
+        print('Must be a number')
+        inpy = input(Questiony)
+    numy=int(inpy)
+    while numy < 1 or numy>4:
+        print('number not valid')
+        inpy = input(Questiony)
+        numy=int(inpy)
+    return numy
 
-def play():
-    x = input('Welche Reihe?')
-    y = input('Welche Zeile?')
-    def check():
-        if field[x][y] == field[x-1][y]:
-            field[x-1][y] == ''
-        if field[x][y] == field[x+1][y]:
-            field[x+1][y] == ''
-        if field[x][y] == field[x][y-1]:
-            field[x][y-1] == ''
-        if field[x][y] == field[x][y+1]:
-            field[x][y+1] == ''
-    check()
+def checkdel_and_double():
+    x = numy
+    y = numx
+    def checkanddel():
+        try:
+            if field[x][y] == field[x-1][y]:
+                field[x-1][y] = 0
+                x-=1
+                checkanddel()
+            if field[x][y] == field[x+1][y]:
+                field[x+1][y] = 0
+                x+=1
+                checkanddel()
+            if field[x][y] == field[x][y-1]:
+                field[x][y-1] = 0
+                y-=1
+                checkanddel() #must still check for new adjacent fields
+            if field[x][y] == field[x][y+1]:
+                field[x][y+1] = 0
+                y+=1
+                checkanddel()
+            return True
+        except:
+            return False
+    if checkanddel() is True:
+        field[x][y] *= 2
+        return True
 
-play()
+for i in range(5):
+    playground()
+    numx = X_Inputcheck('X Axis:')
+    numy = Y_Inputcheck('Y Axis:')
+    checkdel_and_double()
     
