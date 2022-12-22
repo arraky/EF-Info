d3b7bb1451da30f1c1711ad82ef6f130f422b7c5
arraky <shadow.zombey@gmail.com>
2022-12-22 21:41:34 +0100

    Fix email.py

--- What went wrong/2-email.py
+++ What went wrong/2-email.py
@@ -26,6 +26,41 @@ mitglieder = []
 #     ['Foteini', 'Faron']       .
 # ]                              .
 
+mitglieder_raw = verteiler.split('\n')
+for i in range(len(mitglieder_raw)-1):
+    try:
+        if mitglieder_raw[i] == '':
+            mitglieder_raw.pop(i)
+    except:
+        break
 
 
-print(mitglieder)
+mitglieder_no_at = []
+for i in mitglieder_raw:
+    try:
+        mitglieder_no_at.append(i.split('@')[0])
+    except:
+        break
+
+
+
+for i in mitglieder_no_at:
+    mitglieder.append(i.split('.')[::])
+
+
+lenmitglieder= len(mitglieder)
+mitglieder_big =[]
+
+for i in range(lenmitglieder):
+    for j in range(1):
+        mitglieder[i] = [j.title() for j in mitglieder[i]]
+
+        full_name = [j.strip('123456789') for j in mitglieder[i]]
+        
+        mitglieder_big.append(full_name)
+
+for i in range(lenmitglieder):
+    for j in range(2):
+        print(mitglieder_big[i][j], end=' ')
+    print('')
+
